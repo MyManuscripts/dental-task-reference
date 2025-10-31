84fc3227d58a32f932fbc01b0bd764e6624ae529
Komp195 <dmitrykulikov99@gmail.com>
2025-10-31 10:58:02 +0300

    feat: add DatabaseConnection class for managing database connections

---
+++ src/main/java/org/example/dao/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package org.example.dao;
+import java.lang.Class;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:sqlanywhere:Server=d4w;DBN=d4w;UID=dba;PWD=sql";
+    //    - Server: d4w (Server name)
+    //    - DBN: d4w (Database name)
+    //    - UID: dba (User ID)
+    //    - PWD: sql (Password)
+
+
+    static {
+        try {
+            Class.forName("sybase.jdbc4.sqlanywhere.IDriver");
+        }catch (ClassNotFoundException e) {
+            throw new RuntimeException("SQL Anywhere JDBC driver not found", e);
+        }
+    }
+
+    public static Connection getConnection () throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+}
